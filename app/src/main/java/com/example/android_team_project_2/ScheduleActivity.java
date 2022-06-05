b91afb457e972570c2a8c348c48249a899f0e1aa
shshjhjh4455 <shshjhjh4455@gmail.com>
2022-06-05 18:00:16 +0900

    위치찾기
    setMyLocationEnabled 부분에서 오류

--- app/src/main/java/com/example/android_team_project_2/ScheduleActivity.java
+++ app/src/main/java/com/example/android_team_project_2/ScheduleActivity.java
@@ -1,32 +1,88 @@
 package com.example.android_team_project_2;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    //로그캣 사용 설정
+    private static final String TAG = "MainActivity";
 
     int sHour = 0, sMinute = 0, eHour = 0, eMinute = 0;
 
     LatLng hansung = new LatLng(37.5822608, 127.0094254);
 
+    //객체 선언
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    Button btnSearch;
+    EditText editText;
+
+    MarkerOptions myMarker;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        //권한 설정
+        checkDangerousPermissions();
+
+        //객체 초기화
+        editText = findViewById(R.id.editText);
+        btnSearch = findViewById(R.id.search);
+
+        //지도 프래그먼트 설정
+                mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d(TAG, "onMapReady: ");
+                map = googleMap;
+                map.setMyLocationEnabled(true);
+            }
+        });
+        MapsInitializer.initialize(this);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editText.getText().toString().length() > 0) {
+                    Location location = getLocationFromAddress(getApplicationContext(), editText.getText().toString());
+
+                    showCurrentLocation(location);
+                }
+            }
+        });
 
         TimePicker timeStart = findViewById(R.id.timeStart);
         TimePicker timeEnd = findViewById(R.id.timeEnd);
@@ -66,9 +122,83 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         mapFragment.getMapAsync(this);
     }
 
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+        String msg = "Latitutde : " + curPoint.latitude
+                + "\nLongitude : " + curPoint.longitude;
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        //화면 확대, 숫자가 클수록 확대
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        //마커 찍기
+        Location targetLocation = new Location("");
+        targetLocation.setLatitude(37.4937);
+        targetLocation.setLongitude(127.0643);
+        showMyMarker(targetLocation);
+    }
+
+    private void showMyMarker(Location location) {
+        if(myMarker == null) {
+            myMarker = new MarkerOptions();
+            myMarker.position(new LatLng(location.getLatitude(), location.getLongitude()));
+            myMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
+            map.addMarker(myMarker);
+        }
+    }
+
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+
+    private Location getLocationFromAddress(Context context, String address) {
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addresses;
+        Location resLocation = new Location("");
+        try {
+            addresses = geocoder.getFromLocationName(address, 5);
+            if((addresses == null) || (addresses.size() == 0)) {
+                return null;
+            }
+            Address addressLoc = addresses.get(0);
+
+            resLocation.setLatitude(addressLoc.getLatitude());
+            resLocation.setLongitude(addressLoc.getLongitude());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resLocation;
+    }
+
     public void mClick(View view) {
         switch (view.getId()) {
             case R.id.search:
+
                 break;
             case R.id.save:
 
