e78eb6f34ccc42d2e1a6ecac64046a925dc3e383
shshjhjh4455 <shshjhjh4455@gmail.com>
2022-06-06 17:06:43 +0900

    .

--- app/src/main/java/com/example/android_team_project_2/ScheduleActivity.java
+++ app/src/main/java/com/example/android_team_project_2/ScheduleActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_team_project_2;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -21,6 +22,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -46,7 +48,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
     EditText mMemo;
     EditText mPlace;
 
-    private DBHelper mDbHelper;
+    private MyDBHelper myDBHelper;
 
     int sHour = 0, sMinute = 0, eHour = 0, eMinute = 0;
 
@@ -65,11 +67,11 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
-        mId = (EditText)findViewById(R.id.editTitle);
-        mMemo = (EditText)findViewById(R.id.memo);
-        mPlace = (EditText)findViewById(R.id.editText);
+        mId = (EditText) findViewById(R.id.editTitle);
+        mMemo = (EditText) findViewById(R.id.memo);
+        mPlace = (EditText) findViewById(R.id.editText);
 
-        mDbHelper = new DBHelper(this);
+        myDBHelper = new MyDBHelper(this);
 
         //권한 설정
         checkDangerousPermissions();
@@ -79,8 +81,9 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         btnSearch = findViewById(R.id.search);
 
         //지도 프래그먼트 설정
-                mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 Log.d(TAG, "onMapReady: ");
@@ -93,7 +96,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(editText.getText().toString().length() > 0) {
+                if (editText.getText().toString().length() > 0) {
                     Location location = getLocationFromAddress(getApplicationContext(), editText.getText().toString());
 
                     assert location != null;
@@ -113,9 +116,9 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
             public void onTimeChanged(TimePicker timePicker, int h, int m) {
                 sHour = h;
                 sMinute = m;
-                if(sHour <= 22)
+                if (sHour <= 22)
                     timeEnd.setCurrentHour(h + 1);
-                else if(sHour == 23)
+                else if (sHour == 23)
                     timeEnd.setCurrentHour(0);
                 timeEnd.setCurrentMinute(m);
             }
@@ -127,9 +130,9 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
                 eHour = h;
                 eMinute = m;
 
-                if(eHour >= 1)
+                if (eHour >= 1)
                     timeStart.setCurrentHour(h - 1);
-                else if(eHour == 0)
+                else if (eHour == 0)
                     timeStart.setCurrentHour(23);
                 timeEnd.setCurrentMinute(m);
             }
@@ -158,7 +161,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
     }
 
     private void showMyMarker(Location location) {
-        if(myMarker == null) {
+        if (myMarker == null) {
             myMarker = new MarkerOptions();
             myMarker.position(new LatLng(location.getLatitude(), location.getLongitude()));
             myMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
@@ -200,7 +203,7 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
         Location resLocation = new Location("");
         try {
             addresses = geocoder.getFromLocationName(address, 5);
-            if((addresses == null) || (addresses.size() == 0)) {
+            if ((addresses == null) || (addresses.size() == 0)) {
                 return null;
             }
             Address addressLoc = addresses.get(0);
@@ -216,92 +219,36 @@ public class ScheduleActivity extends AppCompatActivity implements OnMapReadyCal
 
     public void mClick(View view) {
         switch (view.getId()) {
+            //지도 찾기
             case R.id.search:
-                insertRecord();
-                viewAllToTextView();
-                viewAllToListView();
+
+
                 break;
             case R.id.save:
-
+                myDBHelper.getAllUsersBySQL();
+                myDBHelper.getAllUsersByMethod();
+                myDBHelper.insertUserBySQL();
+                myDBHelper.insertUserByMethod();
+                myDBHelper.updateUserBySQL();
+                myDBHelper.updateUserByMethod();
                 finish();
                 break;
             case R.id.cancel:
                 finish();
                 break;
             case R.id.delete:
+                myDBHelper.getAllUsersBySQL();
+                myDBHelper.getAllUsersByMethod();
+                myDBHelper.deleteUserBySQL();
+                myDBHelper.updateUserBySQL();
+                myDBHelper.updateUserByMethod();
                 finish();
                 break;
         }
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
-    private void viewAllToListView() {
-
-        Cursor cursor = mDbHelper.getAllUsersByMethod();
-
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),
-                R.layout.item, cursor, new String[]{
-                UserContract.Users._ID,
-                UserContract.Users.KEY_NAME,
-                UserContract.Users.KEY_PHONE},
-                new int[]{R.id._id, R.id.name, R.id.phone}, 0);
-
-        ListView lv = (ListView)findViewById(R.id.listview);
-        lv.setAdapter(adapter);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Adapter adapter = adapterView.getAdapter();
-
-                mId.setText(((Cursor)adapter.getItem(i)).getString(0));
-                mMemo.setText(((Cursor)adapter.getItem(i)).getString(1));
-                mPhone.setText(((Cursor)adapter.getItem(i)).getString(2));
-            }
-        });
-        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-    }
-
-    private void updateRecord() {
-        TextView _id = (TextView)findViewById(R.id._id);
-        TextView name = (TextView)findViewById(R.id.edit_name);
-        TextView phone = (TextView)findViewById(R.id.edit_phone);
-
-        mDbHelper.updateUserBySQL(_id.getText().toString(),name.getText().toString(),phone.getText().toString());
-        long nOfRows = mDbHelper.updateUserByMethod(_id.getText().toString(),
-                name.getText().toString(),
-                phone.getText().toString());
-        if (nOfRows >0)
-            Toast.makeText(this,"Record Updated", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(this,"No Record Updated", Toast.LENGTH_SHORT).show();
-    }
-
-    private void deleteRecord() {
-        TextView _id = (TextView)findViewById(R.id._id);
-
-        mDbHelper.deleteUserBySQL(_id.getText().toString());
-        long nOfRows = mDbHelper.deleteUserByMethod(_id.getText().toString());
-        if (nOfRows >0)
-            Toast.makeText(this,"Record Deleted", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(this,"No Record Deleted", Toast.LENGTH_SHORT).show();
-    }
-
-    private void insertRecord() {
-        TextView title = (TextView)findViewById(R.id.editTitle);
-        TextView memo = (TextView)findViewById(R.id.memo);
-
-        mDbHelper.insertUserBySQL(title.getText().toString(),memo.getText().toString());
-        long nOfRows = mDbHelper.insertUserByMethod(title.getText().toString(),memo.getText().toString());
-        if (nOfRows >0)
-            Toast.makeText(this,nOfRows+" Record Inserted", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(this,"No Record Inserted", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(hansung));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung, 15));
     }
 }
